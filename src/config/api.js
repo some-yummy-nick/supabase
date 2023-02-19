5c69ccf3e1a6b4950ac0dd0ea56650c9a9961066
some.yummy.nick <some.yummy.nick@gmail.com>
2023-02-19 19:52:11 +0300

    fetch data

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log(process.env)
-const supabaseUrl = process.env.API_URL
-const supabaseKey = process.env.API_KEY
+const supabaseUrl = process.env.REACT_APP_URL
+const supabaseKey = process.env.REACT_APP_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,42 @@
-import supabase from '../config/api'
+import { useState, useEffect } from 'react'
+
+import supabase from 'config/api'
+
+//components
+import SmoothieCard from 'components/SmoothieCard/SmoothieCard'
+
 const Home = () => {
-  console.log(supabase)
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from('smoothies').select()
+
+      if (error) {
+        setFetchError('Could not fetch the smoothies')
+        console.error(error)
+        setSmoothies(null)
+      }
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+    fetchSmoothies()
+  }, [])
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothie">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
